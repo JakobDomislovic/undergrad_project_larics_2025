164b3b595fb746b2d15d1e216a30741489dde638
renatodolic <rd55841@fer.hr>
2025-12-15 20:19:05 +0100

    Drone moving towards recognized aruco marker

--- crazyflies/crazyflies_sim/scripts/hover_velocity_demo.py
+++ crazyflies/crazyflies_sim/scripts/hover_velocity_demo.py
@@ -12,10 +12,22 @@ This script demonstrates:
 import rclpy
 from rclpy.node import Node
 import time
+import math
 from builtin_interfaces.msg import Duration
 from crazyflie_interfaces.srv import Takeoff, Land
 from crazyflie_interfaces.msg import Hover
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, PoseStamped
+import tf_transformations
 
+
+def wrap_to_pi(angle):
+    while angle > math.pi:
+        angle -= 2*math.pi
+    while angle < -math.pi:
+        angle += 2*math.pi
+
+    return angle
 
 class HoverVelocityDemoNode(Node):
     def __init__(self):
@@ -23,6 +35,7 @@ class HoverVelocityDemoNode(Node):
         
         self.cf_name = 'cf_1'
         self.hover_height = 1.0
+        self.takeoff = False
         
         self.get_logger().info(f"🚁 Hover Velocity Demo - Crazyflie: {self.cf_name}")
         
@@ -42,8 +55,16 @@ class HoverVelocityDemoNode(Node):
             self.get_logger().info("⏳ Waiting for services...")
             time.sleep(0.5)
         
+        self.aruco_poses = self.create_subscription(
+            PoseStamped,
+            "/aruco_marker_pose",
+            self.move_towards_aruco,
+            1
+        )
+
         self.get_logger().info("✅ Services ready! Starting demo...")
-        self.run_demo()
+        #self.run_demo()
+        #self.run_spiral()
 
     def publish_hover_command(self, vx, vy, yaw_rate):
         """Publish hover command with given velocities."""
@@ -54,6 +75,18 @@ class HoverVelocityDemoNode(Node):
         msg.vy = float(vy)
         msg.yaw_rate = float(yaw_rate)
         msg.z_distance = float(self.hover_height)
+        
+        self.hover_pub.publish(msg)
+
+    def publish_rising_command(self, vx, vy, yaw_rate, z_distance):
+        """Publish hover command with given velocities."""
+        msg = Hover()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = self.cf_name
+        msg.vx = float(vx)
+        msg.vy = float(vy)
+        msg.yaw_rate = float(yaw_rate)
+        msg.z_distance = float(z_distance)
         
         self.hover_pub.publish(msg)
 
@@ -117,9 +150,132 @@ class HoverVelocityDemoNode(Node):
             rclpy.shutdown()
 
 
+    def run_spiral(self):
+        """Moving and spinning the drone in a spiral"""
+        try:
+            z = 1.0
+            # Takeoff
+            self.get_logger().info("🚀 Taking off...")
+            request = Takeoff.Request()
+            request.height = self.hover_height
+            request.duration = Duration(sec=3, nanosec=0)
+            self.takeoff_client.call_async(request)
+            time.sleep(4)  # Wait for takeoff
+            
+            # Publish hovering commands continuously
+            self.get_logger().info("📍 Starting hover with velocity commands...")
+            
+            # Phase 1: Move forward
+            self.get_logger().info("➡️  Moving forward (vx=0.5 m/s)...")
+            for i in range(110):  # 2 seconds at 10 Hz
+                z += 0.05
+                self.publish_rising_command(0.5, 0.0, -1.0, z)
+                time.sleep(0.1)
+            
+            # Phase 5: ZERO velocities but keep hovering!
+            self.get_logger().info("⏸️  Zeroing velocities but keeping hover active...")
+            for i in range(30):  # 3 seconds at 10 Hz with zero velocity
+                self.publish_rising_command(0.0, 0.0, 0.0, z)
+                time.sleep(0.1)
+            
+            # Land
+            self.get_logger().info("🛬 Landing...")
+            request = Land.Request()
+            request.height = 0.0
+            request.duration = Duration(sec=3, nanosec=0)
+            self.land_client.call_async(request)
+            time.sleep(4)
+            
+            self.get_logger().info("✅ Demo complete!")
+            
+        except Exception as e:
+            self.get_logger().error(f"Error: {e}")
+        finally:
+            rclpy.shutdown()
+
+    def move_towards_aruco(self, pose_stamped):
+        """Moving the drone towards an ar tag"""
+        try:
+            quaternion = [pose_stamped.pose.orientation.x, pose_stamped.pose.orientation.y, pose_stamped.pose.orientation.z, pose_stamped.pose.orientation.w]
+            _, _, yaw = tf_transformations.euler_from_quaternion(quaternion)
+
+            yaw = wrap_to_pi(yaw)
+            #self.get_logger().info(f"yaw: {yaw}")
+            vx = 0.0
+            vy = 0.0
+            rotate = 0.0
+
+            if self.takeoff == False:
+            # Takeoff
+                self.get_logger().info("🚀 Taking off...")
+                request = Takeoff.Request()
+                request.height = self.hover_height
+                request.duration = Duration(sec=3, nanosec=0)
+                self.takeoff_client.call_async(request)
+                time.sleep(4)  # Wait for takeoff
+                
+                # Publish hovering commands continuously
+                self.get_logger().info("📍 Starting hover with velocity commands...")
+                self.takeoff = True
+            
+            if pose_stamped.header.frame_id == "invalid":
+                self.publish_rising_command(0.0, 0.0, 0.2, self.hover_height)
+                #time.sleep(0.1)
+                return
+
+            if pose_stamped.pose.position.x < 1.35 or pose_stamped.pose.position.x > 1.65:
+            # Phase 1: Move forward
+                vx = 0.0
+                if pose_stamped.pose.position.x < 1.35:
+                    vx = -0.2
+                else:
+                    vx = 0.2
+                #self.publish_rising_command(vx, 0.0, 0.0, self.hover_height)
+                #time.sleep(0.1)
+
+            if pose_stamped.pose.position.y < -0.07 or pose_stamped.pose.position.y > 0.07:
+            # Phase 1: Move forward
+                vy = 0.0
+                if pose_stamped.pose.position.y < -0.07:
+                    vy = -0.15
+                else:
+                    vy = 0.15
+                #self.publish_rising_command(0.0, vy, 0.0, self.hover_height)
+                #time.sleep(0.1)
+
+            if pose_stamped.pose.position.z < -0.07 or pose_stamped.pose.position.z > 0.07:
+            # Phase 1: Move forward
+                if pose_stamped.pose.position.z < -0.07:
+                    self.hover_height -= 0.01
+                else:
+                    self.hover_height += 0.01
+
+                #self.publish_rising_command(0.0, 0.0, 0.0, self.hover_height)
+                #time.sleep(0.1)
+
+            if yaw < -1.75 or yaw > -1.35:
+                if yaw < -1.75:
+                    rotate = -0.1
+                else:
+                    rotate = 0.1
+
+                #self.publish_rising_command(0.0, 0.0, rotate, self.hover_height)
+                #time.sleep(0.1)
+
+
+                    
+            self.publish_rising_command(vx, vy, rotate, self.hover_height)
+
+            
+        except Exception as e:
+            self.get_logger().error(f"Error: {e}")
+
+
 def main(args=None):
     rclpy.init(args=args)
     node = HoverVelocityDemoNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
     # Node runs demo in __init__ and shuts down
     
 
--- mini_hero_pkg/scripts/process_image.py
+++ mini_hero_pkg/scripts/process_image.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 
 import cv2
 import numpy as np
+import tf_transformations
 
 # ROS2 message imports
 from sensor_msgs.msg import Image, CameraInfo
@@ -21,12 +22,23 @@ from crazyflie_interfaces.msg import Hover
 class ImageProcessingNode(Node):
     def __init__(self):
         super().__init__('image_processing_node')
-
+        
         # State variables
         self.image = None
         self.dist_coeffs = None
         self.camera_matrix = None
         self.bridge = CvBridge()  # For converting between ROS and OpenCV images
+
+
+
+        # Add ArUco setup
+        self.aruco_dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
+        self.aruco_parameters = cv2.aruco.DetectorParameters_create()
+        #self.detector = cv2.aruco.ArucoDetector(self.aruco_dictionary, self.aruco_parameters)
+
+        self.marker_size = 0.25
+
+
 
         # Image subscriber        
         self.image_sub = self.create_subscription(
@@ -51,8 +63,27 @@ class ImageProcessingNode(Node):
             10
         )
 
+
+        #Publisher for aruco location 
+        self.aruco_poses_pub = self.create_publisher(
+            PoseStamped,
+            "/aruco_marker_pose",
+            10
+        )
+
+        self.aruco_marker = self.create_publisher(
+            Marker,
+            "/aruco_marker",
+            10
+        )
+        self.aruco_arrow = self.create_publisher(
+            Marker,
+            "/arruco_arrow",
+            10
+        )
         # Timer for periodic processing with a rate of 10Hz (0.1s delay)
         self.create_timer(0.1, self._run_loop)
+
         
     def _run_loop(self):
         """Main loop to process images."""
@@ -60,15 +91,118 @@ class ImageProcessingNode(Node):
         # Image and Image information is grabbed and stored in callbacks.
         # See camera_info_callback and image_callback
 
-        # Check if image and camera info is received
-        if self.image is not None or self.camera_matrix is not None:
-            
-            #__________ TODO: Process the image here
-            # You have access to self.image, self.camera_matrix, and self.dist_coeffs
-            # Process them according to given tasks and publish a new image to a topic /new_image.
 
-            # Replace the following line with actual image processing you want to perform
+            
+        # Check if image and camera info is received
+        if self.image is not None and self.camera_matrix is not None:
+            
+
+            gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+            corners, ids, rejected = cv2.aruco.detectMarkers(
+                gray,
+                self.aruco_dictionary,
+                parameters=self.aruco_parameters
+            )
+
+            if ids is None:
+                pose_stamped = PoseStamped()
+                pose_stamped.header.frame_id = "invalid"
+                self.aruco_poses_pub.publish(pose_stamped)
+                return
+
+
+            
+            cv2.aruco.drawDetectedMarkers(self.image, corners, ids)
+            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+                corners,
+                self.marker_size,
+                self.camera_matrix,
+                self.dist_coeffs,
+            )
+
+            #self.get_logger().info(f"tvecs: {tvecs},\nrvecs: {rvecs}")
             processed_image = self.image
+
+            for idx, corner in enumerate(corners):
+                pose_marker = Marker()
+                pose_marker.header.stamp = self.get_clock().now().to_msg()
+                pose_marker.header.frame_id = "cf_1"
+
+                pose_marker.type = Marker.ARROW
+
+                start_point = Point()
+                start_point.x = 0.0
+                start_point.y = 0.0
+                start_point.z = 0.0
+
+                #rotating the original position:
+                # x = z
+                # y = -x
+                # z = -y
+                T = np.array([
+                    [0, 0, 1],
+                    [-1, 0, 0],
+                    [0, -1, 0]
+                ])
+                x, y, z = T @ tvecs[idx][0]
+                end_point = Point()
+                end_point.x = x
+                end_point.y = y
+                end_point.z = z
+
+                pose_marker.points.append(start_point)
+                pose_marker.points.append(end_point)
+
+                pose_marker.scale.x = 0.02
+                pose_marker.scale.y = 0.04
+                pose_marker.scale.z = 0.1
+
+                pose_marker.color.r = 1.0
+                pose_marker.color.a = 1.0
+
+
+                R, _ = cv2.Rodrigues(rvecs[idx])
+
+                R_ros = T @ R
+                quaternion = tf_transformations.quaternion_from_matrix(
+                    np.vstack((np.hstack((R_ros, np.array([[0], [0], [0]]))), [0, 0, 0, 1]))
+                )
+                pose_stamped = PoseStamped()
+                #first, second, yaw = tf_transformations.euler_from_quaternion(quaternion)
+
+                #self.get_logger().info(f"first: {first}\nsecond: {second}\nyaw: {yaw}")
+                pose_stamped.pose.position = end_point
+                pose_stamped.pose.orientation.x = float(quaternion[0])
+                pose_stamped.pose.orientation.y = float(quaternion[1])
+                pose_stamped.pose.orientation.z = float(quaternion[2])
+                pose_stamped.pose.orientation.w = float(quaternion[3])
+
+                pose_stamped.header.frame_id = "cf_1"
+                pose_stamped.header.stamp = self.get_clock().now().to_msg()
+               # self.get_logger().info(f"position: {pose_stamped}")
+                self.aruco_poses_pub.publish(pose_stamped)
+                
+                self.aruco_arrow.publish(pose_marker)
+
+
+                marker = Marker()
+                marker.header.frame_id = "cf_1"
+                marker.header.stamp = self.get_clock().now().to_msg()
+                marker.type = Marker.CUBE
+                marker.action = Marker.ADD
+
+                marker.pose = pose_stamped.pose
+                marker.scale.x = self.marker_size
+                marker.scale.y = self.marker_size
+                marker.scale.z = 0.002
+
+                marker.color.r = 0.0
+                marker.color.g = 1.0
+                marker.color.a = 0.8
+                self.aruco_marker.publish(marker)
+
+
 
             # Publish the processed image on /processed_image topic
             self.publish_processed_image(processed_image)
@@ -83,7 +217,6 @@ class ImageProcessingNode(Node):
             self.camera_matrix = np.array(msg.k).reshape(3, 3)
             self.dist_coeffs = np.array(msg.d)
             self.get_logger().info(f"Camera matrix:\n{self.camera_matrix}")
-
 
     def image_callback(self, msg):
         """Process camera image and detect ArUco markers."""
